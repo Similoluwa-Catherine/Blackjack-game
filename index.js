ac12784e6fc059b43e3002103306bb8ef96deffa
Similoluwa Catherine <catherinesimiloluwa@gmail.com>
2021-12-10 00:47:23 -1200

    Update index.js

--- index.js
+++ index.js
@@ -4,14 +4,15 @@ let player = {
 };
 
 let cards = [];
-let sum = 0;
+let sum = " ";
 let hasBlackJack = false;
 let isAlive = false;
 let message = "";
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
 let cardsEl = document.getElementById("cards-el");
-
+let startBtn = document.getElementById("btn");
+let newBtn = document.getElementById("new-btn");
 
 let playerEl = document.getElementById("player-el");
 playerEl.textContent = player.name + ": $" + player.chips;
@@ -46,22 +47,45 @@ function renderGame() {
     };
 
     sumEl.textContent = "Sum: " + sum;
-    if (sum <= 20) {
+    
+    if (sum <= 20 && player.chips !== 0) {
         message = "Do you want to draw a new card?";
+        newBtn.disabled = false;
+        newBtn.style.background = "burlywood";
     } else if (sum === 21) {
         message = "You've got a Blackjack";
         isAlive = true;
         hasBlackJack = true;
         player.chips = player.chips + 20;
-        playerEl.textContent = player.name + ": $" + player.chips;
-
-    } else if(sum > 21) {
+        playerEl.textContent = player.name + ": $" + player.chips + ".";
+        let showMessage = " You just won $20";
+        playerEl.textContent += showMessage;
+        alert(showMessage);
+        newBtn.disabled = true;
+        newBtn.style.background = "grey";
+    } else if(sum > 21 && player.chips > 0) {
         message = "You're out of the game";
         isAlive = false;
         player.chips = player.chips - 10;
-        playerEl.textContent = player.name + ": $" + player.chips;
+        playerEl.textContent = player.name + ": $" + player.chips + ".";
+        let showMessage = " You just lost $10";
+        playerEl.textContent += showMessage;
+        alert(showMessage);
+        newBtn.disabled = true;
+        newBtn.style.background = "grey";
     };
     messageEl.textContent = message;
+
+    if (player.chips === 0) {
+        startBtn.disabled = true;
+        newBtn.disabled = true;
+        message = " You've exhausted your money"
+        alert(message);
+        startBtn.style.background = "grey";
+        newBtn.style.background= "grey";
+        playerEl.textContent = player.name + ": $" + player.chips + ".";
+        playerEl.textContent += message;
+    }
 };
 
 function newCard() {
